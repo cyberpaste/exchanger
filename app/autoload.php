49e0478a00a52d25d3f78dc6dcc80b669a7035de
cyberpaste <proovit08@gmail.com>
2018-12-19 00:32:55 +0300

    Update autoload.php

--- app/autoload.php
+++ app/autoload.php
@@ -1,10 +1,20 @@
 <?php
 
 spl_autoload_register(function ($class) {
-    $path = __DIR__ . '/' . $class . '.php';
-    
+    $class = explode('\\', $class);
+    $i = 0;
+    $c = count($class);
+    $realclass = "";
+    foreach ($class as $ns) {
+        if ($c - 1 == $i) {
+            $realclass .= $ns;
+        } else {
+            $realclass .= strtolower($ns) . '\\';
+        }
+        $i++;
+    }
+    $path = __DIR__ . '/' . $realclass . '.php';
     $path = str_replace('\\', '/', $path);
-
     if (file_exists($path)) {
         require_once $path;
     }
